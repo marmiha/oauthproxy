cfa92f225c481642f39c19769f4780c28543ee56
Miha Markež <miha.markez@gmail.com>
2023-04-07 20:37:46 +0200

    chore: remove unwanted _build2

--- web/_build/_app/immutable/chunks/stores.327e777a.js
+++
@@ -1,1 +0,0 @@
-import"./index.9bc38fcc.js";import{s as e}from"./singletons.2be0ebc8.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};

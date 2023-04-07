d383afe940cab539e02c79e71c9b9d70af39787c
marmiha <marmiha@users.noreply.github.com>
2023-04-07 19:18:29 +0000

    chore: build svelte frontend

---
+++ web/build/_app/immutable/chunks/1.50d42d80.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.1fa59e21.js";export{t as component};
---
+++ web/build/_app/immutable/chunks/2.5e2bc24f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.2fbc4df7.js";export{t as component};
